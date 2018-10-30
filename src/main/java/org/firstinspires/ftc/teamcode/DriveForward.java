164bba6050fce33f8ad5b03a5c089062ce07b3a4
Team 10058 <10058dms@gmail.com>
2018-10-30 16:40:06 -0400

    initial commit

---
+++ src/main/java/org/firstinspires/ftc/teamcode/DriveForward.java
@@ -0,0 +1,197 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Servo.Direction;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Drive Forward", group="Linear Opmode")
+//@Disabled
+public class DriveForward extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    private DcMotor joint1 = null;
+    private DcMotor joint2 = null;
+    private DcMotor roller = null;
+    private Servo pusher = null;
+
+    final  double pusher_home = 0.0;//Servo starting position
+    final  double pusher_min_range = - 0.5;
+    final  double pusher_max_range = 0.5;
+
+    @Override
+     public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_motor");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_motor");
+
+        joint1 = hardwareMap.get(DcMotor.class, "joint_1");
+        joint2 = hardwareMap.get (DcMotor.class, "joint_2");
+        roller = hardwareMap.get (DcMotor.class, "roller");
+        pusher.setDirection(Servo.Direction.FORWARD);
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            //leftPower  = -gamepad1.left_stick_y ;
+            //rightPower = -gamepad1.right_stick_y ;
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            //control joint1
+            double joint1Power = -gamepad2.left_stick_y;
+            if (joint1Power > .75 || joint1Power < -.75) {
+
+                //the constant number sets the max power
+                 joint1.setPower(joint1Power * 0.3);
+
+            } else {
+
+                joint1.setPower(0.0);
+
+            }
+
+            //control joint2
+            double joint2Power = -gamepad2.right_stick_y;
+            if (joint2Power > 0.8 || joint2Power < -0.8) {
+
+                //the constant number sets the max power
+                joint2.setPower(joint2Power);
+
+            } else {
+
+                joint2.setPower(0.0);
+
+            }
+
+            //control roller
+            double rollerIn = gamepad2.right_trigger;
+            double rollerOut = gamepad2.left_trigger;
+            //if right trigger is pressed down enough. dominates over roll out
+            if (rollerIn > 0.5) {
+
+                roller.setPower(0.4);
+
+                //if left trigger is pressed enough
+            } else if (rollerOut > 0.5) {
+
+                roller.setPower(-.4);
+
+            } else {
+
+                roller.setPower(0.0);
+
+            }
+                double   pusher = pusher_home;
+
+               final double pusher_speed = 0.01;
+
+               if (gamepad2.right_bumper)
+               pusher += pusher_speed;
+
+               else if (gamepad2.left_bumper)
+                   pusher -= pusher_speed;
+            pusher = Range.clip(pusherposition, pusher_min_range, pusher_max_range  );
+            pusher.setPosition(pusherposition);
+               // run until the end of the match (driver presses STOP)
+            double tgtPower = 0;
+            {
+                tgtPower = -this.gamepad1.left_stick_y;
+                // check to see if we need to move the servo.
+                if (gamepad1.y) {
+                    // move to 0 degrees.
+                    pusher.setPosition(0);
+                } else if (gamepad1.x || gamepad1.b) {
+                    // move to 90 degrees.
+                    pusher.setPosition(0.5);
+                } else if (gamepad1.a) {
+                    // move to 180 degrees.
+                    pusher.setPosition(1);
+                }
+                telemetry.addData("Servo Position", pusher.getPosition());
+                telemetry.addData("Target Power", tgtPower);
+                telemetry.addData("Status", "Running");
+                telemetry.update();
+                // Show the elapsed game time and wheel power.
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                telemetry.addData("pusher", "%2f", pusherposition);
+                telemetry.update();
+            }}}}
